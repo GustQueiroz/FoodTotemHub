af249af214ea89aafabb0b6be2f793c055172e76
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2025-02-24 19:49:06 -0300

    categories component

---
+++ src/app/[slug]/menu/components/categories.tsx
@@ -0,0 +1,31 @@
+import { Restaurant } from "@prisma/client";
+import { ClockIcon } from "lucide-react";
+import Image from "next/image";
+interface RestaurantCategoriesProps {
+  restaurant: Restaurant;
+}
+
+const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
+  return (
+    <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border bg-white p-5">
+      <div className="flex items-center gap-3">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={45}
+          height={45}
+        />
+        <div>
+          <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+          <p className="text-xs text-gray-500">{restaurant.description}</p>
+        </div>
+      </div>
+      <div className="mt-3 flex items-center gap-1 text-sm text-green-500">
+        <ClockIcon size={16} />
+        <p>Aberto!</p>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCategories;
