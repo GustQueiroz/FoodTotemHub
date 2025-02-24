cefa37fcdeb16d7f4797abf658fb3f3a5f264368
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2025-02-24 16:25:42 -0300

    tela inicial

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { db } from "@/lib/prisma";
 
 interface RestaurantPageProps {
@@ -23,6 +25,41 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
           height={82}
         />
         <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 pt-14">
+        <Card>
+          <CardContent className="flex flex-col items-center gap-8 py-8">
+            <Image
+              src="/dineIn.png"
+              alt="Para comer aqui"
+              width={78}
+              height={80}
+            />
+            <Button variant="secondary" className="rounded-full">
+              Para comer aqui
+            </Button>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="flex flex-col items-center gap-8 py-8">
+            <Image
+              src="/delivery.png"
+              alt="Para levar"
+              width={78}
+              height={80}
+            />
+            <Button variant="secondary" className="rounded-full">
+              Para levar
+            </Button>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
