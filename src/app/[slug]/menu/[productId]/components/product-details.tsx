c34d797bd5739f96e8f0d8fc589f7b568033a2b5
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2025-02-25 16:39:55 -0300

    tela produto estilização geral

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface ProductDetailsProps {
     product: Prisma.ProductGetPayload<{ include: { restaurant: {select: {name: true, avatarImageUrl: true}} } }>;
@@ -23,14 +24,14 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
         setQuantity(quantity + 1);
     }
     return ( 
-        <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col rounded-t-3xl p-5">
-            <div className="flex-auto">
+        <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col rounded-t-3xl p-5 overflow-hidden">
+            <div className="flex-auto overflow-hidden">
                 <div className="flex items-center gap-1.5">
                     <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full" /> 
                 <p className="text-xs text-muted-foreground">{product.restaurant.name}</p>
                 </div>
                 <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between mt-3">
                 <h3 className="text-xl font-semibold">               
                     {new Intl.NumberFormat("pt-BR", {
                     style: "currency",
@@ -47,6 +48,7 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
                     </Button>
                 </div>
                 </div>
+                <ScrollArea className="h-full">
                 <div className="mt-6 space-y-3">
                     <h4 className="font-semibold">Sobre</h4>
                     <p className="text-sm text-muted-foreground">{product.description}</p>
@@ -56,9 +58,13 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
                         <ChefHatIcon size={18} />                  
                         <h4 className="font-semibold">Ingredientes</h4>
                     </div>
-                    <p className="text-sm text-muted-foreground">{product.ingredients}</p>
+                    <ul className="list-disc px-5 text-sm text-muted-foreground">
+                        {product.ingredients.map((ingredient) => (
+                            <li key={ingredient} className="text-sm text-muted-foreground">{ingredient}</li>
+                        ))}
+                    </ul>
                 </div>
-                
+                </ScrollArea>
             </div>
             <Button className="w-full mt-6 rounded-full">Adicionar à sacola</Button>
         </div>
