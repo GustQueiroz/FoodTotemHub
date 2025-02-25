a8e7c51b505dc8c1e5918deeab45169b83374124
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2025-02-24 21:43:30 -0300

    productpage route

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -1,9 +1,25 @@
-const ProductPage = () => {
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+interface ProductPageProps {
+    params: Promise<{ slug: string, productId: string }>;
+}
+
+const ProductPage = async ({ params }: ProductPageProps) => {
+    const { slug, productId } = await params;
+    const product = await db.product.findUnique({ where: { id: productId } });
+    if (!product) {
+        return notFound();
+    }
     return ( 
         <div>
-            <h1>Product</h1>
+            <h1>{slug}</h1>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
         </div>
-     );
+     ); 
 }
  
 export default ProductPage;
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,14 +1,16 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 interface ProductsProps {
   products: Product[];
 }
 
 const Products = ({ products }: ProductsProps) => {
+  const {slug} = useParams<{slug: string}>();
   return <div className="space-y-3 px-5 py-3">
     {products.map((product) => (
-    <Link href={`/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b" key={product.id}>
+    <Link href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b" key={product.id}>
       <div>
         <h3 className="text-sm font-medium">{product.name}</h3>
         <p className="text-sm text-gray-500 line-clamp-2 text-muted-foreground">{product.description}</p>
