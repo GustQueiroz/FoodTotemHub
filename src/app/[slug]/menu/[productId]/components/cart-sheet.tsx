19aab47e77b946d23b7d8c0bda3735ffc4384060
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2025-02-28 20:07:40 -0300

    button finalizar compra

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
@@ -17,10 +18,13 @@ const CartSheet = () => {
         <SheetHeader>
           <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
-        <div className="py-5">
-          {products.map((product) => (
-            <CartProductItem key={product.id} product={product} />
-          ))}
+        <div className="flex h-full flex-col py-5">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CartProductItem key={product.id} product={product} />
+            ))}
+          </div>
+          <Button className="w-full rounded-full">Finalizar pedido</Button>
         </div>
       </SheetContent>
     </Sheet>
