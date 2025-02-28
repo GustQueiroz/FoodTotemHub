bba420deabfb81830eb77d980505d018c4905c79
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2025-02-28 19:57:06 -0300

    product cart

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -61,13 +61,13 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const decreaseProductQuantity = (productId: string) => {
     setProducts((prevProducts) => {
       return prevProducts.map((prevProducts) => {
-        if (prevProducts.id === productId) {
-          if (prevProducts.quantity === 1) {
-            return prevProducts;
-          }
-          return { ...prevProducts, quantity: prevProducts.quantity - 1 };
+        if (prevProducts.id != productId) {
+          return prevProducts;
         }
-        return prevProducts;
+        if (prevProducts.quantity === 1) {
+          return prevProducts;
+        }
+        return { ...prevProducts, quantity: prevProducts.quantity - 1 };
       });
     });
   };
